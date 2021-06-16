70a93627008f9deb8a61bb50896e21211da0736c
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-06-16 10:42:35 -0400

    Update pine.js

--- src/pine/pine.js
+++ src/pine/pine.js
@@ -1,6 +1,6 @@
 var getFile = require('./finder/finder.js');
 var exec = require('child_process').exec;
-var chalk = require('chalk')
+var chalk = require('chalk');
 
 function makePine() {
   var data = getFile();
@@ -9,22 +9,23 @@ function makePine() {
     if (process.argv[3] != undefined) {
       if (eval(`data.pines.${process.argv[3]}`) == undefined) {
         console.log("Could not find task");
-        process.exit(0)
+        process.exit(0);
       }
       else {
         console.log(chalk.blue.bold(`Running the ${process.argv[3]} pine in Pinefile '${name}'`));
-        var toBash = eval(`data.pines.${process.argv[3]}`)
+        var toBash = eval(`data.pines.${process.argv[3]}`);
         console.log(chalk.blue.bold(`Running: <${toBash}>`));
+        console.log();
         exec(toBash, function (err, stdout, stderr) {
           if (err) {
-            console.log(err)
+            console.log(err);
           }
           else {
             if (stderr) {
-              console.log(stderr)
+              console.log(stderr);
             }
             else {
-              console.log(stdout)
+              console.log(stdout);
             }
           }
         });
